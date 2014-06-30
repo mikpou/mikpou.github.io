21b8576c0e3b7a6d73481ebb342597cac028ad64
Mikko Poutanen <mikko.poutanen@polidea.com>
2014-06-30 15:34:57 +0200

    Load post

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+$(document).ready(function() {
+	
+	$("ul.posts li a.post-link").on('click', function(e) {
+		e.preventDefault();
+		$(this).siblings(".post-content").load($(this).attr("href"), ".post");
+		// return false;
+	});
+});
