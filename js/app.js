f956bede18a2efae0bed5085fc3ddfcd2e318c5b
Mikko Poutanen <mikko.poutanen@polidea.com>
2014-07-02 14:10:45 +0200

    Enabled logo fadeout for screen sizes > 1024

--- js/app.js
+++ js/app.js
@@ -2,21 +2,16 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 // $(document).foundation();
 $(document).ready(function() {
-	// $(document).on("scroll", function(){
-	// 	yPos = $(this).scrollTop();
-	// 	var logo = $(".header-row img");
-	// 	// if (logo.css('opacity') > 0) {
-	// 		logo.css({'opacity': (1 - yPos/240)});
-	// 		console.log(yPos);
-	// 		console.log(window.pageYOffset);
-	// 	// } 
-	// });
-
-	// $(document).on('touchstart', function() {
-	// 	yPos = $(this).scrollTop();
-	// 	var logo = $(".header-row img");
-	// 	// if (logo.css('opacity') > 0) {
-	// 		logo.css({'opacity': (1 - yPos/240)});
-	// 		console.log(yPos);
-	// });
+	var documentW = $(document).width();
+	if (documentW > 1024) {
+		$(document).on("scroll", function(){
+			yPos = $(this).scrollTop();
+			var logo = $(".header-row img");
+			// if (logo.css('opacity') > 0) {
+				logo.css({'opacity': (1 - yPos/240)});
+				console.log(yPos);
+				console.log(window.pageYOffset);
+			// } 
+		});
+	}	
 });
