d7aadf8355179b700e5b738a95a1648b0aaadf04
Mikko Poutanen <mikko.poutanen@polidea.com>
2014-07-02 16:03:55 +0200

    Adjustments

--- js/app.js
+++ js/app.js
@@ -2,16 +2,13 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 // $(document).foundation();
 $(document).ready(function() {
+	var yPos;
+	var logo = $(".header-row img");
 	var documentW = $(document).width();
 	if (documentW > 1024) {
-		$(document).on("scroll", function(){
+		$(document).on("scroll touchmove", function(){
 			yPos = $(this).scrollTop();
-			var logo = $(".header-row img");
-			// if (logo.css('opacity') > 0) {
-				logo.css({'opacity': (1 - yPos/240)});
-				console.log(yPos);
-				console.log(window.pageYOffset);
-			// } 
+			logo.css({'opacity': (1 - yPos/240)});		
 		});
 	}	
 });
